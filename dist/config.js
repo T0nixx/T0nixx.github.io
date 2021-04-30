99d5d84368680901dd1c5cb6afc537682a3cb3c4
Jiseok Oh <jiseok95@gmail.com>
2021-04-30 09:07:08 +0900

    remove: dist from .gitignore

---
+++ dist/config.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WEB_API_KEY = void 0;
+exports.WEB_API_KEY = 'F6F227F3DD558E55B8130D309383021C';
+//# sourceMappingURL=config.js.map
